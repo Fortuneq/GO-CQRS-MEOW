0d5eac54096539b57175b4b86d71f418decfbd22
Vlad <vladvolkov03@mail.ru>
2022-07-22 01:28:04 +0300

    this finally came

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container py-5">
+    <div class="row mb-4">
+      <h1 class="col-12">Meower</h1>
+    </div>
+    <div class="row">
+      <Timeline class="col" />
+      <Search class="col" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Timeline from '@/components/Timeline';
+import Search from '@/components/Search';
+
+export default {
+  components: {
+    Timeline,
+    Search,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 768px;
+}
+</style>
